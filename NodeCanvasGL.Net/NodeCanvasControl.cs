63de2a1da8122fa2e5a881ca3a3e482eedec04e2
Geert S. <101360451+Iso83@users.noreply.github.com>
2026-03-08 22:25:35 +0100

    Add node and connect pass-through APIs to NodeCanvasControl

--- NodeCanvasGL.Net/NodeCanvasControl.cs
+++ NodeCanvasGL.Net/NodeCanvasControl.cs
@@ -4,19 +4,28 @@ namespace NodeCanvasGL.Net;
 
 public class NodeCanvasControl : UserControl
 {
-    private Engine? _engine;
+    private readonly Engine _engine;
 
     public NodeCanvasControl()
     {
         _engine = new Engine();
     }
 
+    public uint CreateNode(string type, float x, float y)
+    {
+        return _engine.CreateNode(type, x, y);
+    }
+
+    public void Connect(uint a, uint ac, uint b, uint bc)
+    {
+        _engine.Connect(a, ac, b, bc);
+    }
+
     protected override void Dispose(bool disposing)
     {
         if (disposing)
         {
-            _engine?.Dispose();
-            _engine = null;
+            _engine.Dispose();
         }
 
         base.Dispose(disposing);
