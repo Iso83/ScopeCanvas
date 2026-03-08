405b5d1f922c7c24fbeafb55a2073dd02b9d846f
Geert S. <101360451+Iso83@users.noreply.github.com>
2026-03-08 21:30:29 +0100

    Add C API engine create/destroy lifecycle functions

--- include/nodecanvas_c_api.h
+++ include/nodecanvas_c_api.h
@@ -4,6 +4,9 @@ extern "C" {
 
 typedef struct NC_Engine NC_Engine;
 
+NC_Engine* nc_engine_create();
+void nc_engine_destroy(NC_Engine* engine);
+
 #ifdef __cplusplus
 }
 #endif
--- src/c_api/nodecanvas_c_api.cpp
+++ src/c_api/nodecanvas_c_api.cpp
@@ -2,6 +2,33 @@
 
 #include "Engine/CanvasEngine.h"
 
+#include <new>
+
 struct NC_Engine {
     CanvasEngine* engine = nullptr;
 };
+
+NC_Engine* nc_engine_create() {
+    NC_Engine* handle = new (std::nothrow) NC_Engine;
+    if (handle == nullptr) {
+        return nullptr;
+    }
+
+    handle->engine = new (std::nothrow) CanvasEngine;
+    if (handle->engine == nullptr) {
+        delete handle;
+        return nullptr;
+    }
+
+    return handle;
+}
+
+void nc_engine_destroy(NC_Engine* engine) {
+    if (engine == nullptr) {
+        return;
+    }
+
+    delete engine->engine;
+    engine->engine = nullptr;
+    delete engine;
+}
